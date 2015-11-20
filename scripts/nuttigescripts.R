6573840f069c792ca6383c44a8b84d5965b1f104
Roel Hogervorst <hogervorst.rm@gmail.com>
2015-11-20 16:23:49 +0100

    Added new functions to display min, max, mode and median of variable

--- scripts/nuttigescripts.R
+++ scripts/nuttigescripts.R
@@ -13,6 +13,14 @@ if (is.data.frame(df) == TRUE){
         info$class <-lapply(df, class)
         info$type<-lapply(df, typeof)
         info$N_of_missing<- lapply(df, function(x) sum(is.na(x)))
+        info$unique<-lapply(df, function(x) length(unique(x)))
+        info<-cbind(info, min=apply (df, 2, function(x) which(x == min(x, na.rm = T))[1]))
+        info<-cbind(info, max=apply (df, 2, function(x) which(x == max(x, na.rm = T))[1]))
+        info<-cbind(info, mode= apply(test,2, function(x) {
+                ux <- unique(x)
+                ux[which.max(tabulate(match(x, ux)))]
+        }) )
+        info<-cbind(info, median=apply (df, 2, function(x) which(x == median(x, na.rm = T))[1]))
         return(info)
 }  else {
         print("not a data frame!")
@@ -21,5 +29,9 @@ if (is.data.frame(df) == TRUE){
 #aan te passen met
 #Voor numerieke variabelen N, M, SD, range
 #Voor categorische variabelen N, freq + percentage per categorie
+#als date, laagste en hoogste
+#als character vaak voorkomenste
+# als factor alle factoren?
+#
 
 
